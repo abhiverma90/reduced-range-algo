ce25477e8899515db4f1846dcafb34fa38d646a7
abhiverma90 <51044334+abhiverma90@users.noreply.github.com>
2019-05-26 13:57:53 +0530

    @added : minimized ranges for restricted zipcode ranges.

---
+++ nisumcodingtest/src/com/nisum/zipcode/model/Ranges.java
@@ -0,0 +1,35 @@
+package com.nisum.zipcode.model;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Ranges<T extends Comparable<T>> implements Comparator<List<T>> {
+
+	private ArrayList<Integer> range = new ArrayList<Integer>();
+
+	public ArrayList<Integer> getRange() {
+		return range;
+	}
+
+	public void addRange(int max, int min) {
+		this.range.add(max);
+		this.range.add(min);
+	}
+
+	@Override
+	public int compare(List<T> o1, List<T> o2) {
+		for (int index = 0; index < Math.min(o1.size(), o2.size()); index++) {
+			int c = o1.get(index).compareTo(o2.get(index));
+			if (c != 0) {
+				return c;
+			}
+		}
+		return Integer.compare(o1.size(), o2.size());
+	}
+
+	@Override
+	public String toString() {
+		return "Ranges [range=" + range + "]";
+	}
+}
---
+++ nisumcodingtest/src/com/nisum/zipcode/processor/RangeProcessor.java
@@ -0,0 +1,62 @@
+package com.nisum.zipcode.processor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+import com.nisum.zipcode.model.Ranges;
+
+public class RangeProcessor {
+
+	public static List<Ranges<Integer>> getReducedRanges(List<ArrayList<Integer>> rangesToReduce) {
+		getSortedZipcodeRanges(rangesToReduce);
+		List<Ranges<Integer>> reducedRanges = extractRanges(rangesToReduce);
+		return reducedRanges;
+	}
+
+	private static List<Ranges<Integer>> extractRanges(List<ArrayList<Integer>> rangesToReduce) {
+		ListIterator<ArrayList<Integer>> listIterator = rangesToReduce.listIterator();
+		List<Ranges<Integer>> reducedRanges = new ArrayList<Ranges<Integer>>();
+		boolean[] traversedRangeList = new boolean[rangesToReduce.size()];
+		while (listIterator.hasNext()) {
+			List<Integer> ranges = listIterator.next();
+			int nextIndex = listIterator.nextIndex();
+			boolean flag = traversedRangeList[nextIndex - 1];
+			if (flag) {
+				continue;
+			}
+			int min = ranges.get(0);
+			int max = ranges.get(1);
+
+			int nextMin = rangesToReduce.get(nextIndex).get(0);
+			int nextMax = rangesToReduce.get(nextIndex).get(1);
+
+			if (max >= nextMin) {
+				Ranges<Integer> ranges1 = new Ranges<Integer>();
+				if (max >= nextMax) {
+					ranges1.addRange(max, min);
+					reducedRanges.add(ranges1);
+				} else {
+					ranges1.addRange(nextMax, min);
+					reducedRanges.add(ranges1);
+				}
+				traversedRangeList[nextIndex] = true;
+
+			} else {
+				Ranges<Integer> ranges2 = new Ranges<Integer>();
+				ranges2.getRange().addAll(ranges);
+				reducedRanges.add(ranges2);
+			}
+		}
+		return reducedRanges;
+	}
+
+	private static void getSortedZipcodeRanges(List<ArrayList<Integer>> rangesToReduce) {
+		Collections.sort(rangesToReduce, new Ranges<Integer>());
+		for(List<Integer> ranges: rangesToReduce) {
+			Collections.sort(ranges);
+		}
+	}
+
+}
